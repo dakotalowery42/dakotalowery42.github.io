3e061cacfdc7b23ef596e0b0c693bcf97203cc27
dakotalowery42 <dakota.lowery89@icloud.com>
2021-10-20 21:35:28 -0400

    Updated Python HTML Page image

--- Lab6.js
+++ Lab6.js
@@ -16,57 +16,57 @@ $("#btn").on('click', function(){
         if(weather == 'clear sky'){
             $('body').css({"background-color":"LightYellow"})
             var clearskyImg = document.createElement('img');
-            clearskyImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/clearsky.png";
+            clearskyImg.src = "clearsky.png";
             document.getElementById('div1').appendChild(clearskyImg);
         }else if(weather == 'broken clouds'){
             $('body').css({"background-color":"DarkGray"})
             var brokencloudsImg = document.createElement('img');
-            brokencloudsImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/broeknclouds.png";
+            brokencloudsImg.src = "broeknclouds.png";
             document.getElementById('div1').appendChild(brokencloudsImg);
         }else if(weather == 'scattered clouds'){
             $('body').css({"background-color":"Gray"})
             var scatteredcloudsImg = document.createElement('img');
-            scatteredcloudsImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/scatteredclouds.png";
+            scatteredcloudsImg.src = "scatteredclouds.png";
             document.getElementById('div1').appendChild( scatteredcloudsImg);
         }else if(weather == 'overcast clouds'){
             $('body').css({"background-color":"Gray"})
             var overcastcloudsImg = document.createElement('img');
-            overcastcloudsImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/overcastclouds.png";
+            overcastcloudsImg.src = "overcastclouds.png";
             document.getElementById('div1').appendChild(overcastcloudsImg);
         }else if(weather == 'few clouds'){
             $('body').css({"background-color":"LightGray"})
             var fewcloudsImg = document.createElement('img');
-            fewcloudsImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/fewclouds.png";
+            fewcloudsImg.src = "fewclouds.png";
             document.getElementById('div1').appendChild(fewcloudsImg);
         }else if(weather == 'shower rain'){
             $('body').css({"background-color":"Blue"})
             var showerrainImg = document.createElement('img');
-            showerrainImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/showerrain.png";
+            showerrainImg.src = "showerrain.png";
             document.getElementById('div1').appendChild(showerrainImg);
         }else if(weather == 'rain'){
         $('body').css({"background-color":"DarkBlue"})
             var rainImg = document.createElement('img');
-            rainImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/rain.png";
+            rainImg.src = "rain.png";
             document.getElementById('div1').appendChild(rainImg);
         }else if(weather == 'thunderstorm'){
             $('body').css({"background-color":"DarkBlue"})
             var thunderstormImg = document.createElement('img');
-            thunderstormImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/thunderstorm.png";
+            thunderstormImg.src = "thunderstorm.png";
             document.getElementById('div1').appendChild(thunderstormImg);
         }else if(weather == 'snow'){
             $('body').css({"background-color":"Gainsboro"})
             var snowImg = document.createElement('img');
-            snowImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/snow.png";
+            snowImg.src = "snow.png";
             document.getElementById('div1').appendChild(snowImg);
         }else if(weather == 'mist'){
         $('body').css({"background-color":"LightBlue"})
             var mistImg = document.createElement('img');
-            mistImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/mist.png";
+            mistImg.src = "mist.png";
             document.getElementById('div1').appendChild(mistImg);
         }else if(weather == 'light rain'){
             $('body').css({"background-color":"LightBlue"})
             var lightrainImg = document.createElement('img');
-            lightrainImg.src = "https://github.com/dakotalowery42/dakotalowery42.github.io/blob/9c3791cf0f0ff6d32b4790b09554970cb0b9cd52/weathericons/lightrain.png";
+            lightrainImg.src = "lightrain.png";
             document.getElementById('div1').appendChild(lightrainImg);
         }
     })
